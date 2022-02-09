ec5958feaf31fb72c921ec1b37b4b25f42fdab97
Lex <nrazam95@gmail.com>
2022-02-09 22:35:32 +0800

    user

--- src/api/users/controllers/users.controller.ts
+++ src/api/users/controllers/users.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { Request } from 'express';
+// import { Request } from 'express';
+import { GetUser } from 'src/api/auth/strategies/get-user.decorator';
+import { JwtAuthGuard } from 'src/guard/jwt-auth.guard';
 import { PasswordHelper } from '../../auth/password/password.helper';
 import { UsersService } from '../services/users.service';
 
@@ -12,13 +14,23 @@ export class UsersController {
     private readonly usersService: UsersService,
   ) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get('')
-  getUsers(@Req() req: Request) {
-    return this.usersService.findByUsername(req.query.username);
+  getUsers(@GetUser() user: any) {
+    const username: string = user.username;
+    return this.usersService.findByUsername(username);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('')
   createPassword(@Body() body: any) {
     return this.passwordHelper.hashPassword(body.password);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':slug')
+  getUser(@Param('slug') slug: string) {
+    const username: string = slug;
+    return this.usersService.findByUsername(username);
   }
 }
